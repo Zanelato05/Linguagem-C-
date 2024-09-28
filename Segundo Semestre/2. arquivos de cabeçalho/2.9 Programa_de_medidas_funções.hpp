d71e93201130380a34ddb97de7508ae824514d26
Lucas Zanelato Salvadego <zanelatohandebol@gmail.com>
2024-09-28 18:14:41 -0300

    Delete Segundo Semestre directory

--- Segundo Semestre/2. arquivos de cabeçalho/2.9 Programa_de_medidas_funções.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef Programa_de_medidas_funções_hpp
-#define Programa_de_medidas_funções_hpp
-double obterPeso();
-double obterAltura();
-double obterIdade();
-double calculoImc(double peso, double altura);
-double calculoTmb(double peso, double altura, double idade);
-double calculoAgua(double peso);
-#endif
--- Segundo Semestre/GERENCIAMNETO DE BIBLIOTECA/src/usuarios.cpp
+++
@@ -1,7 +0,0 @@
-#include "../include/usuarios.hpp"
-
-vector<userStruct> usuarios = {
-    {"João", "joao123", "mynbi@example.com", "47123456789", ""},
-    {"Maria", "maria456", "ztejd@example.com", "47987654321", ""},
-    {"Carlos", "carlos789", "rdlnk@example.com", "47555555555", ""},
-};
